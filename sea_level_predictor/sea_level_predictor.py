506cdeb2dc2b313851a65c7286683c02ec772063
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-05-02 16:37:53 +0100

    Update sea_level_predictor.py
    completed second line of best fit

--- sea_level_predictor/sea_level_predictor.py
+++ sea_level_predictor/sea_level_predictor.py
@@ -17,13 +17,28 @@ def draw_plot():
     firstLineX = range(df['Year'][0], 2050)
     firstLineY = firstLineX * slope + y_intercept
     #plotting regression line
-    plt.plot(firstLineX, firstLineY, color='red', label='Regression line')
+    plt.plot(firstLineX, firstLineY, color='red')
+
     # Create second line of best fit
+    #Plot a new line of best fit just using the data from year 2000 through the most recent year in the dataset.
+    #Make the line also go through the year 2050 to predict the sea level rise in 2050 if the rate of rise continues 
+    #as it has since the year 2000.
+    df2  = df[(df['Year'] >= 2000)]
+    #reset index so we are able to get the first row in line 30
+    df2.reset_index(drop=True, inplace=True)
+    slope2, y_intercept2, _, _, _ = linregress(df2['Year'], df2['CSIRO Adjusted Sea Level'])
+    secondLineX = range(df2['Year'][0], 2050)
+    secondLineY = secondLineX * slope2 + y_intercept2
+    
+    plt.plot(secondLineX, secondLineY, color='green')
 
 
     # Add labels and title
 
-    
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
+    plt.title("Rise in Sea Level")
+
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
