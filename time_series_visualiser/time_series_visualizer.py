7257e10c12c73d2b0ce6cd96a420039811d30ab3
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-05-02 14:38:24 +0100

    Update time_series_visualizer.py
    draw_line_plot() function completed

--- time_series_visualiser/time_series_visualizer.py
+++ time_series_visualiser/time_series_visualizer.py
@@ -5,18 +5,23 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("/workspace/boilerplate-page-view-time-series-visualizer/fcc-forum-pageviews.csv")
-
+df = pd.read_csv("/workspace/boilerplate-page-view-time-series-visualizer/fcc-forum-pageviews.csv",index_col='date', parse_dates=True)
+#Clean the data by filtering out days when the page views were in the top 2.5% of the dataset or bottom 2.5% of the dataset.
 # Clean data
-df = None
-
+df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
+# print(df)
 
 def draw_line_plot():
+    #making a copy of dataframe
+    df_for_line = df
+    #defining the figure and the size of the subplot
+    fig, ax = plt.subplots(figsize=(20, 8))
     # Draw line plot
-
-
-
-
+    line = ax.plot(df_for_line)
+    
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
