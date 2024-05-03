f76aff5fcd269524b1e6fccb672be0fe81622bf6
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-05-03 10:14:55 +0100

    Update mean_var_std.py
    mean of array implemented

--- mean_var_std/mean_var_std.py
+++ mean_var_std/mean_var_std.py
@@ -5,8 +5,14 @@ def calculate(list):
         matrix = np.array(list).reshape(3,3)
     except ValueError:
         print("List must contain nine numbers.")
+    #print(matrix)
 
+    mean = [np.mean(matrix,axis=0), np.mean(matrix,axis=1), np.mean(matrix)]
 
+    calculations = {
+        'mean': [i.tolist() for i in mean]
+    }
 
+    print(calculations)
 
     return calculations
